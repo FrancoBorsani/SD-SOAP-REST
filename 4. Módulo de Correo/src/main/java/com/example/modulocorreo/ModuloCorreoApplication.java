db692497dd89a347d1f1ca7b45b2ff53651c692e
FrancoAguirre644 <francoaguirre644@gmail.com>
2021-10-13 13:54:57 -0300

    Configuración del proyecto - Módulo de correo

---
+++ 4. Módulo de Correo/src/main/java/com/example/modulocorreo/ModuloCorreoApplication.java
@@ -0,0 +1,13 @@
+package com.example.modulocorreo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloCorreoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloCorreoApplication.class, args);
+	}
+
+}
---
+++ 4. Módulo de Correo/src/test/java/com/example/modulocorreo/ModuloCorreoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modulocorreo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloCorreoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
