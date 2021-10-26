005e48308c6f0369254aaff810590b1f14aaff44
FrancoBorsani <borsafranco@gmail.com>
2021-10-25 22:43:33 -0300

    React template

---
+++ Tienda/src/views/index-sections/Notifications.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+// reactstrap components
+import { Alert, Container } from "reactstrap";
+
+// core components
+
+function Notifications() {
+  const [alert1, setAlert1] = React.useState(true);
+  const [alert2, setAlert2] = React.useState(true);
+  const [alert3, setAlert3] = React.useState(true);
+  const [alert4, setAlert4] = React.useState(true);
+  return (
+    <>
+      <div className="section section-notifications">
+        
+      </div>
+    </>
+  );
+}
+
+export default Notifications;
---
+++ Tienda/src/views/index-sections/Tabs.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  NavItem,
+  NavLink,
+  Nav,
+  TabContent,
+  TabPane,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+// core components
+
+function Tabs() {
+  const [iconPills, setIconPills] = React.useState("1");
+  const [pills, setPills] = React.useState("1");
+  return (
+    <>
+      <div className="section section-tabs">
+        <Container>
+          
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default Tabs;
