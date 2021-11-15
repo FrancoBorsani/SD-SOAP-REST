09647e184503c46b3e9447df92628d16609c12f4
FrancoAguirre644 <francoaguirre644@gmail.com>
2021-11-15 15:38:06 -0300

    Maquetado de cuentas bancarias - tienda

--- Tienda/src/components/Profile/BankAccounts.js
+++ Tienda/src/components/Profile/BankAccounts.js
@@ -1,20 +1,18 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import { useContext } from "react";
-import { useState } from "react";
 import { DataContext } from "store/GlobalState";
-import { getData } from "utils/fetchData";
-import { postData } from "utils/fetchData";
 
-const CreditCards = () => {
+const BankAccounts = () => {
 
     const initialState = {
-        tipo: '',
+        banco: '',
         numero: '',
     }
 
-    const [card, setCard] = useState(initialState);
+    const [bankAccount, setBankAccount] = useState(initialState);
 
-    const [cards, setCards] = useState([]);
+    const [bankAccounts, setBankAccounts] = useState([]);
 
     const { state, dispatch } = useContext(DataContext);
 
@@ -22,42 +20,55 @@ const CreditCards = () => {
 
     useEffect(() => {
 
-        getData(`tarjeta/getTarjetasDeUsuario`, auth.token)
+        /******** ACA TENDRIA QUE TRAER LAS CUENTAS BANCARIAS DEL VENDEDOR *******/
+
+        /*
+
+        getData(`tarjeta/getCuentasBancariasDelUsuario`, auth.token)
             .then(res => {
-                setCards(res);
+                setBankAccounts(res);
             })
-            .catch(e => console.log(e))
+            .catch(e => console.log(e)) 
+
+        */
+
 
     }, [auth.token]);
 
     const handleChangeInput = e => {
         const { name, value } = e.target;
-        setCard({ ...card, [name]: value });
+        setBankAccount({ ...bankAccount, [name]: value });
     }
 
     const handleSubmit = async e => {
 
         e.preventDefault();
 
+        /******** ACA TENDRIA QUE GUARDAR LA NUEVA CUENTA BANCARIA DEL VENDEDOR *******/
+
+        /*
+
         const response = await postData('tarjeta/agregar', card, auth.token);
 
-        if (response.error) return alert(response.message)
+        if (response.error) return alert(response.message) 
+
+        setBankAccounts([...bankAccounts, response]); 
         
-        setCards([...cards, response]);
+        */
 
     }
 
     return (
         <div className="tab-pane fade show active" id="account" role="tabpanel" aria-labelledby="account-tab">
-            <h3 className="mb-4">Tarjetas</h3>
+            <h3 className="mb-4">Cuentas Bancarias</h3>
             <div className="row">
                 <div className="col-md-12">
 
                     {
-                        cards.length === 0 && (
+                        bankAccounts.length === 0 && (
                             <div className="card">
                                 <div className="card-body">
-                                    Usted no posee tarjetas registradas.
+                                    Usted no posee cuentas bancarias registradas.
                                 </div>
                             </div>
                         )
@@ -66,9 +77,9 @@ const CreditCards = () => {
 
                     <ul className="list-group w-100 mb-4">
                         {
-                            cards.length > 0 &&
-                            cards.map(card => (
-                                <li className="list-group-item" key={card.id}>{card.numero + ' - ' +  card.tipo}</li>
+                            bankAccounts.length > 0 &&
+                            bankAccounts.map(bankAccount => (
+                                <li className="list-group-item" key={bankAccount.id}>{bankAccount.numero + ' - ' + bankAccount.banco}</li>
                             ))
                         }
                     </ul>
@@ -78,32 +89,29 @@ const CreditCards = () => {
                 <form className="card-body row" onSubmit={handleSubmit}>
                     <div className="col-md-6">
                         <div className="form-group">
-                            <label>Tipo</label>
-                            <select className="form-control" 
-                                name="tipo"
+                            <label>Banco</label>
+                            <input type="text" className="form-control"
+                                name="banco"
+                                value={bankAccount.numero}
                                 onChange={handleChangeInput}
-                            >
-                            <option value="" disabled selected hidden>Seleccione el tipo de tarjeta</option>
-                            <option value="credito">Credito</option>
-                            <option value="debito">Debito</option>
-                            </select>
+                            />
                         </div>
                     </div>
                     <div className="col-md-6">
                         <div className="form-group">
-                            <label>Número de Tarjeta</label>
+                            <label>Número</label>
                             <input type="text" className="form-control"
                                 name="numero"
-                                value={card.numero}
-                                onChange={handleChangeInput} 
+                                value={bankAccount.numero}
+                                onChange={handleChangeInput}
                             />
                         </div>
                     </div>
-                    <button className="btn btn-primary btn-round w-100 mx-3" type="submit">Agregar tarjeta</button>
+                    <button className="btn btn-primary btn-round w-100 mx-3" type="submit">Agregar cuenta</button>
                 </form>
             </div>
         </div>
     )
 }
 
-export default CreditCards;
+export default BankAccounts;
